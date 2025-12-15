a00719641d2776fb1c681ca07774da2a6bd652aa
gskresh <svreshu@gmail.com>
2025-12-15 11:30:08 +0100

    Update affichage.h

--- affichage.h
+++ affichage.h
@@ -3,20 +3,28 @@
 
 #include <stdio.h>
 
-/*
- * Affiche les informations du niveau :
- * - coups restants
- * - score actuel
- * - objectifs restants (tableau de 5 pierres)
- *
- * Entrees :
- *   - int coups : nombre de coups restants
- *   - int score : score actuel du joueur
- *   - int objectif[5] : tableau contenant les objectifs restant pour chaque pierre
- *
- * Sortie :
- *   - aucune (void)
- */
-void afficherInfosNiveau(int coups, int score, int temps, int vies, int objectif[5]);
+/* Structure pour les informations du niveau */
+typedef struct {
+    int vies;
+    int coups;
+    int temps;
+    int objectifs[5];
+    int score;
+} InfosNiveau;
+
+/* Affiche le menu principal */
+void afficherMenu(void);
+
+/* Affiche le plateau 9x9 */
+void afficherPlateau(int plateau[9][9], int curseurX, int curseurY, int selectionActive);
+
+/* Affiche les informations du niveau */
+void afficherInfosNiveau(int coupsRestants, int score, int objectifs[5], char* nomsPierres[5]);
+
+/* Lit une touche clavier (z q s d espace p) */
+char lireInputClavier(void);
+
+/* Efface l’écran (Windows) */
+void effacerEcran(void);
 
 #endif
